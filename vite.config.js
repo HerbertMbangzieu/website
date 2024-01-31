b2bba5b563167ae26fa89daadb6c3c49e60df87e
Herbert Mbangzieu <herbertmbangzieu@gmail.com>
2024-01-31 11:42:58 -0500

    update-vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "HerbertMbangzieu/website/",
+  base: "https://herbertmbangzieu.github.io/website/",
   plugins: [react()],
 })
